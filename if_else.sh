eed0732b8b37d010c9f1b9c38f734a433b6da9d8
Shakvilla <abdulshakuraclement@yahoo.com>
2023-01-23 23:40:51 +0000

    if else statement added

---
+++ if_else.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+$count = 10
+
+if (( $count < 9 ))
+then
+	echo "the condition is true"
+elif (( $count <= 9 ))
+
+	echo "the condition is true"
+
+else 
+	echo "the condition is false"
+
+fi
+
